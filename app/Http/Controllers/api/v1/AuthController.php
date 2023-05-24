ba7ee8fbcb1fa15f52d965883156b033e0c8fe95
willam-gm <will-gm@hotmail.com>
2023-05-23 22:40:43 -0400

    Instalacion y configuracion de passport

---
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request)
+    {
+        $requestData  = $request->validate([
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:50',
+            'password' => 'required|confirmed'
+        ]);
+
+        $requestData['password'] = bcrypt($request->password);
+        $user = User::create($requestData);
+        $accessToken = $user->createToken('AuthToken')->accessToken;
+
+        return response([
+            'user' => $user,
+            'access_token' => $accessToken
+        ], 201);
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Email or password is invalid'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([
+            'user' => $user,
+            'access_token' => $accessToken
+        ], 200);
+    }
+
+    public function list(Request $request)
+    {
+        $users = User::all();
+        return response($users);
+    }
+}
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -20,13 +20,20 @@ class RouteServiceProvider extends ServiceProvider
     public const HOME = '/home';
 
     /**
+     * the constant to defaine API Version.
+     * 
+     * @var string
+     */
+    private const API_VERSION = 'v1';
+
+    /**
      * The controller namespace for the application.
      *
      * When present, controller route declarations will automatically be prefixed with this namespace.
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -38,10 +45,10 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::prefix('api/' . self::API_VERSION)
                 ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                ->namespace($this->namespace . '\\api\\' . self::API_VERSION)
+                ->group(base_path('routes/api/' . self::API_VERSION . '/api.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)
